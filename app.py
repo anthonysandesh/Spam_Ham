6af85fe61c325ebcdab6b399bbf462105eca6512
antony <52021528+antonysandesh@users.noreply.github.com>
2021-02-21 16:11:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ text_model.fit(x,y)
 select = st.text_input('Enter your message')
 op = text_model.predict([select])
 st.title(op[0])
-
+st.write("For getting span 'copy below sentence and paste it' or write something like this:" )
+st.write("Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005. Text FA to 87121 to receive entry question(std txt rate)T&C's apply 08452810075over18's")
